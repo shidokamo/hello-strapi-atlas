d83fabf9a58d459081b8414e67215c9d1f3211f3
Kamome Shido <shidokamo@users.noreply.github.com>
2020-01-03 17:09:38 +0000

    IP ホワイトリストのサブコマンドを修正。単体テストで動作確認済み

--- bin/atlas-post-white-list.sh
+++ bin/atlas-post-white-list.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-printf "$*" | jq --slurp -s -R 'split(" ") | map(split(" ")) | map({"ip": .[0]})'
+json=$(jq -n --arg ip "$*" '$ip | split(" ") | map(split(" ")) | map({"cidrBlock": .[0], "comment": "AWS ip range"})')
+curl --user "${ATLAS_PUBLIC_KEY}:${ATLAS_PRIVATE_KEY}" --digest --include \
+     --header "Accept: application/json" \
+     --header "Content-Type: application/json" \
+     --request POST "https://cloud.mongodb.com/api/atlas/v1.0/groups/${ATLAS_PROJECT_ID}/whitelist?pretty=true" \
+     --data "$json"
