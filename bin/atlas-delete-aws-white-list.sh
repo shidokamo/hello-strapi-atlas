fe8fc1e6d8fd34b82528c55a80899f6cc66f348a
Kamome Shido <shidokamo@users.noreply.github.com>
2020-01-04 02:06:06 +0000

    AWS の region を指定できるようにする。

--- bin/atlas-delete-aws-white-list.sh
+++ bin/atlas-delete-aws-white-list.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
-# You might need to add wait time for the API call
+# Remove IP white list for specified AWS region
+
+region=$1
 
 # Remove all existing AWS ips ("/" should be replaced with %2F in DELETE request)
 ips=$(
   wget -O - https://ip-ranges.amazonaws.com/ip-ranges.json \
-  |jq -r '.prefixes[].ip_prefix | sub("/"; "%2F")' \
+  |jq --arg region "$region" -r '.prefixes[] | select(.region==$region) | .ip_prefix | sub("/"; "%2F")' \
   |sort
 )
+
+# You might need to add wait time for the API call
 for ip in $ips; do
   curl --user "${ATLAS_PUBLIC_KEY}:${ATLAS_PRIVATE_KEY}" --digest --include \
        --header "Accept: application/json" \
