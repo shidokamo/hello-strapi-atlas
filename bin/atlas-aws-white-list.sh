525eda126ad6be3fb0d6896d8917043d529dcf0c
Kamome Shido <shidokamo@users.noreply.github.com>
2020-01-04 06:21:55 +0000

    Region 関連の動作を修正

--- bin/atlas-aws-white-list.sh
+++ bin/atlas-aws-white-list.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
+
+json=$(
+  wget -O - https://ip-ranges.amazonaws.com/ip-ranges.json \
+  |jq -r '.prefixes[] | select(.region=| [{"cidrBlock": .ip_prefix, "comment": "AWS ip range"}]'
+)
+echo
+
 # Create group of ip white list to avoid API rate limit violation
-# Get JSON --> generate ip list --> Pass each 100 ips to sub command
+region=$1
+# Get JSON --> generate ip list for specified region --> Pass each 100 ips to sub command
 wget -O - https://ip-ranges.amazonaws.com/ip-ranges.json \
-|jq -r ".prefixes[].ip_prefix" \
+|jq --arg region ${region} -r '.prefixes[] | select(.region==$region) | .ip_prefix' \
+|sort \
 |tr -s '\n' '\0' \
 |xargs -0 -n 50 -t `dirname $0`/atlas-post-white-list.sh
 
